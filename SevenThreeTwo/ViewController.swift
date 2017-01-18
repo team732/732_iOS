ddaf836f0e6d3c87dca78180c0644b5c6bc71b6d
윤민섭 <yoon@yunminseob-ui-MacBook-Pro.local>
2017-01-18 23:21:56 +0900

    732팀 첫 프로젝트

---
+++ SevenThreeTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SevenThreeTwo
+//
+//  Created by 윤민섭 on 2017. 1. 18..
+//  Copyright © 2017년 윤민섭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
