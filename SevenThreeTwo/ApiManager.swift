7c1fae72a092ab27e2f222258f3382607d058ffa
skrth00 <skrth00@naver.com>
2017-02-10 04:45:20 +0900

    commentviewcontroller & api manager & model 추가

--- SevenThreeTwo/ApiManager.swift
+++ SevenThreeTwo/ApiManager.swift
@@ -25,8 +25,43 @@ class ApiManager {
     //completion:(String) -> Void (ex)
     func request(completion : @escaping (JSON)->Void){
         Alamofire.request(url,method: method,parameters: parameters).responseJSON{ response in
-            let info = JSON(response.result.value!)
-            completion(info)
+            //            let info = JSON(response.result.value!)
+            //            completion(info)
+            switch(response.result) {
+            case .success(_):
+                if response.result.value != nil{
+                    
+                    //print(response.result.value!)
+                    
+                    let json = JSON(response.result.value!)
+                    
+                    //print(json)
+                    //print(json["data"]["contents"][0].arrayValue)
+                    
+                    print(json["data"]["contents"].array!.count)
+                    print(json["pagination"]["nextUrl"].string!)
+                    
+                    print(json["data"]["contents"][0]["contentId"].int!.description)
+                    print(json["data"]["contents"][0]["content"]["picture"].string!)
+                    print(json["data"]["contents"][0]["content"]["text"].string!)
+                    print(json["data"]["contents"][0]["userId"].int!.description)
+                    
+                    print(json["data"]["contents"][0]["nickname"].string!)
+                    print(json["data"]["contents"][0]["popularity"].int!.description)
+                    print(json["data"]["contents"][0]["missionId"].int!.description)
+                    print(json["data"]["contents"][0]["createdAt"].string!)
+                    print(json["data"]["contents"][0]["likeCount"].int!.description)
+                    print(json["data"]["contents"][0]["mission"]["text"].string!)
+                    
+                    
+                }
+                break
+                
+            case .failure(_):
+                
+                break
+                
+            }
         }
     }
     
@@ -35,10 +70,10 @@ class ApiManager {
     /*
      사용 하는 컨트롤러에서
      
-     apiManager.request() { (info) in 
-        print(info) -> info 는 JSON 형식 (@escaping 옆 () 안에 String 을 넣어주면 info 는 String으로 반환 단, completion안에도 String으로 넣어줘야함. 즉 이곳에서 내가 원하는 값으로 반환 받는것
+     apiManager.request() { (info) in
+     print(info) -> info 는 JSON 형식 (@escaping 옆 () 안에 String 을 넣어주면 info 는 String으로 반환 단, completion안에도 String으로 넣어줘야함. 즉 이곳에서 내가 원하는 값으로 반환 받는것
      }
-    */
+     */
     
     
     
